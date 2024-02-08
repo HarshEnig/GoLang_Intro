ee8057d59a5f9757b74f3b92145fedc440988748
harshc <programmerhc02@gmail.com>
2024-02-08 23:17:16 +0530

    third commit with var

--- hello_world.go
+++ hello_world.go
@@ -8,4 +8,11 @@ func main() {
 
 	//this is a comment
 	/*also a comment*/
+	var student1 string = "Harsh"
+	var student2 = "Jane"
+	x := 2
+
+	fmt.Println(student1)
+	fmt.Println(student2)
+	fmt.Println(x)
 }
